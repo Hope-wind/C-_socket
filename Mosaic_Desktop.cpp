0c7d9b5c39706a20b79bfe137655bfd9705f9dcf
Wind <71535035+Hope-wind@users.noreply.github.com>
2021-02-04 11:37:31 +0800

    Update Mosaic_Desktop.cpp

--- Mosaic_Desktop.cpp
+++ Mosaic_Desktop.cpp
@@ -6,82 +6,75 @@
 
 #define NUM 1024
 
-void Mosaic_Desktop()
-{
-	HWND hwnd = GetDesktopWindow();
-	int rects[NULL][4];
-	while(true)
-	{
-
-		
-		if(LockWindowUpdate(hwnd) == false)
-		{
-				return 1;
-		}
-
-		//获得桌面窗口的设备上下文
-		HDC hdcSrc = GetDCEx(hwnd,NULL,
-						DCX_CACHE |  //高速缓存)
-						DCX_LOCKWINDOWUPDATE);
-		
-		//创建一个内存设备上下文
-		HDC hdcMem = CreateCompatibleDC(hdcSrc); 
-
-		//切割小块的宽度
-		
-		int cx = GetSystemMetrics(SM_CXSCREEN) / 10;
-		int cy = GetSystemMetrics(SM_CYSCREEN) / 10;
-
-		//创建位图
-		HBITMAP hBitmap = CreateCompatibleBitmap(hdcSrc,cx,cy);
-
-		//位图和内存设备的关联
-		SelectObject(hdcMem,hBitmap);
-
-		//切割区域
-		//随机数
-		
-		
-		for(int i = 0;i < NUM;i++)
-		{
-			rects[i][0] = cx * (rand() % 10);
-			rects[i][1] = cy * (rand() % 10);
-			rects[i][2] = cx * (rand() % 10);
-			rects[i][3] = cy * (rand() % 10);
-			
-		}
-
-		//切割
-		for(int i = 0;i < NUM;i++)
-		{
-				BitBlt(
-								hdcMem,
-								0,0,
-								cx,cy,
-								hdcSrc,
-								rects[i][0],
-								rects[i][1],
-								SRCCOPY //拷贝方式
-					  );
-
-				BitBlt(hdcSrc,rects[i][2],rects[i][3],cx,cy,hdcMem,0,0,SRCCOPY);
-				Sleep(1);
-				
-				//打扫门户
-				DeleteDC(hdcMem);
-				ReleaseDC(hwnd,hdcSrc);
-				DeleteObject(hBitmap);
-
-
-		}
-		
-		
-}
-
 int main()
 {
-    Mosaic_Desktop();
-    return 0;
-}
-  
+		HWND hwnd = GetDesktopWindow();
+		int rects[NULL][4];
+		while(true)
+		{
 
+
+			if(LockWindowUpdate(hwnd) == false)
+			{
+					return 1;
+			}
+
+			//获得桌面窗口的设备上下文
+			HDC hdcSrc = GetDCEx(hwnd,NULL,
+							DCX_CACHE |  //高速缓存)
+							DCX_LOCKWINDOWUPDATE);
+
+			//创建一个内存设备上下文
+			HDC hdcMem = CreateCompatibleDC(hdcSrc); 
+
+			//切割小块的宽度
+
+			int cx = GetSystemMetrics(SM_CXSCREEN) / 10;
+			int cy = GetSystemMetrics(SM_CYSCREEN) / 10;
+
+			//创建位图
+			HBITMAP hBitmap = CreateCompatibleBitmap(hdcSrc,cx,cy);
+
+			//位图和内存设备的关联
+			SelectObject(hdcMem,hBitmap);
+
+			//切割区域
+			//随机数
+
+
+			for(int i = 0;i < NUM;i++)
+			{
+				rects[i][0] = cx * (rand() % 10);
+				rects[i][1] = cy * (rand() % 10);
+				rects[i][2] = cx * (rand() % 10);
+				rects[i][3] = cy * (rand() % 10);
+
+			}
+
+			//切割
+			for(int i = 0;i < NUM;i++)
+			{
+					BitBlt(
+									hdcMem,
+									0,0,
+									cx,cy,
+									hdcSrc,
+									rects[i][0],
+									rects[i][1],
+									SRCCOPY //拷贝方式
+						  );
+
+					BitBlt(hdcSrc,rects[i][2],rects[i][3],cx,cy,hdcMem,0,0,SRCCOPY);
+					Sleep(1);
+
+					//打扫门户
+					DeleteDC(hdcMem);
+					ReleaseDC(hwnd,hdcSrc);
+					DeleteObject(hBitmap);
+					
+
+			}
+
+		
+	}
+}
